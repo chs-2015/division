d0c430e76318fc14a479cf772b2dfaf09df540e4
Brandon Berney <brandonberney@protonmail.com>
2015-09-14 13:56:12 -0400

    COMMENTS!

--- src/division.java
+++ src/division.java
@@ -6,23 +6,29 @@
  
 import java.util.Scanner;
 public class projectDivision {
-    
     public static void main(String[] args) {
+    	// Initialize our variables. (they're very self-explanatory)
     	int firstNumber, secondNumber, largerNumber, smallerNumber, quotient, remainder;
     	Scanner kbReader = new Scanner(System.in);
 
-		System.out.println("Pfeffer only wants the largest number/the smallest number. Watch out! :-(");
+		// Warn & query the user
+		System.out.println("Pfeffer only wants the largest number divided by the smallest number. Watch out! :-(");
 		System.out.print("Enter your first number: ");
 		firstNumber = kbReader.nextInt();
 
 		System.out.print("Enter your second number: ");
 		secondNumber = kbReader.nextInt();
 
+		// Determine the largest and smallest number
 		largerNumber = Math.max(firstNumber, secondNumber);
 		smallerNumber = Math.min(firstNumber, secondNumber);
+
+		// Do some math
 		System.out.println("Dividing " + largerNumber + " by " + smallerNumber + "...");
 		quotient = largerNumber / smallerNumber;
 		remainder = largerNumber % smallerNumber;
+
+		// Return results to the user
 		System.out.println("Results: " + quotient + " R" + remainder);
     }
 }
